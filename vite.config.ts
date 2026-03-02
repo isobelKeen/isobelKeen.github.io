108f9a61beee833fee788d8ed3ded2d1bcda11f6
IsobelKeen <isobel.keen@gmail.com>
2026-03-02 15:53:36 +0100

    added vite vitamin base project and edited package.json and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
+
 export default defineConfig(() => ({
+	base: '/isobelKeen.github.io/',
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
